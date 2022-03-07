2592e3aebda5f3baeecc19e759dc8a2c44e4bd06
Pasquale Troise <troisepasquale1990@gmail.com>
2022-03-07 02:51:00 +0000

    Create labels for date inputs

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -9,7 +9,7 @@ export const SearchBar = ({ searchBarCb }) => {
     // Retrieve the provided dates
     const start = e.target.querySelector('[name=start]').value
     const end = e.target.querySelector('[name=end]').value   
-    
+
     // Convert the dates in ISOS datetime format
     const isosStart = new Date(start).toISOString()
     const isosEnd = new Date(end).toISOString()
@@ -21,14 +21,20 @@ export const SearchBar = ({ searchBarCb }) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input  type="date" 
-                placeholder="start date"
-                name='start'/>
-
-        <input  type="date" 
-                placeholder="end date"
-                name='end'/>
-
+        <div>
+          <label htmlFor="start">Start date:</label><br/>
+          <input  type="date" 
+                  placeholder="start date"
+                  name='start'
+                  required/>
+        </div>
+        <div>
+          <label htmlFor="end">End date:</label><br/>
+          <input  type="date" 
+                  placeholder="end date"
+                  name='end'
+                  required/>
+        </div>
         <button>Submit</button>
       </form>   
     </>
