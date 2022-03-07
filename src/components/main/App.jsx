4ba46085442f7b65be1c74f364fbe4ecec736220
Pasquale Troise <troisepasquale1990@gmail.com>
2022-03-07 02:26:37 +0000

    Update app component

---
+++ src/components/main/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import { SearchBar } from '../searchBar/searchBar'
+import { TableLaunches } from '../tableLaunches/tableLaunches';
+
+function App() {
+  const [ dates, setDates ] = useState({})
+  const [ launchesTotal, setLaunchesTotal ] = useState(0)
+
+  const containerStyle = {
+    margin: '24px auto',
+    maxWidth: '1024px',
+  }
+
+  const searchBarCb =({ start, end }) => {
+    setDates(prevState => {
+      return { ...prevState, 'startDate': start, 'endDate': end }
+    })
+  }
+
+  const totalLaunchesCb = (total) => { setLaunchesTotal(total) }
+
+  return (
+    <div className="App" style={containerStyle} >
+      <SearchBar searchBarCb={searchBarCb}/>
+      <p style={{float: 'left', marginTop: '48px'}}>Founded <strong>{launchesTotal}</strong> launches:</p>
+      <TableLaunches  dates={dates} 
+                      totalLaunchesCb = {totalLaunchesCb} />
+    </div>
+  );
+}
+
+export default App;
