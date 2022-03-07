8bab72331e29763b38d9473477db21cabf0f0884
Pasquale Troise <troisepasquale1990@gmail.com>
2022-03-07 02:50:19 +0000

    update import

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SearchBar } from '../searchBar/searchBar'
 import { TableLaunches } from '../tableLaunches/tableLaunches';
 
