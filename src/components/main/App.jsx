af64cec1afeaf03e1904f6f454fbee18c3a6c689
Pasquale Troise <troisepasquale1990@gmail.com>
2022-03-07 15:08:09 +0000

    Import MapLaunches component and pass setState for coordinates

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { useState } from 'react';
 import { SearchBar } from '../searchBar/SearchBar'
-import { TableLaunches } from '../tableLaunches/TableLaunches';
+import { TableLaunches } from '../tableLaunches/TableLaunches'
 import { ChartLaunches } from '../chartLaunches/ChartLaunches'
+import { MapLaunches } from '../mapLaunches/MapLaunches'
 
 function App() {
   const [ dates, setDates ] = useState({})
   const [ launchesTotal, setLaunchesTotal ] = useState(0)
   const [ locations, setLocations ] = useState([])
+  const [ coordinates, setCoordinates ] = useState([])
 
   const containerStyle = {
     margin: '24px auto',
@@ -26,9 +28,11 @@ function App() {
       <p style={{float: 'left', marginTop: '48px'}}>Founded <strong>{launchesTotal}</strong> launches:</p>
       <TableLaunches  dates={dates} 
                       setLaunchesTotal={setLaunchesTotal} 
-                      setLocations={setLocations}/>
+                      setLocations={setLocations}
+                      setCoordinates={setCoordinates}/>
 
       <ChartLaunches  locations={locations}/>
+      <MapLaunches coordinates={coordinates}/>
     </div>
   );
 }
